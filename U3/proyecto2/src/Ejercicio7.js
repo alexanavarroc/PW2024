706c9ab2b75d4f1534d7efe78c76198f5d841dd3
alexanavarroc <alexa.navarrocalderon@cananea.tecnm.mx>
2024-03-13 18:59:35 -0700

    ejercicio7

---
+++ U3/proyecto2/src/Ejercicio7.js
@@ -0,0 +1,8 @@
+export default function Ejercicio7() {
+  return (
+    <>
+      <h1>En este componente del Ejercicio 7</h1>
+      <a href="/"> Regresar</a>
+    </>
+  );
+}
--- U3/proyecto2/src/index.js
+++ U3/proyecto2/src/index.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './index.css';
 import App from './App';
+import Ejercicio7 from './Ejercicio7';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/ejercicio7" element={<Ejercicio7 />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
